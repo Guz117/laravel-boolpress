752d6f59337608246c33b84dd29fde493bfa8469
Guz117 <cristian.guzman117@gmail.com>
2022-03-01 15:27:13 +0100

    userInfo

--- app/User.php
+++ app/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+
     /**
      * The attributes that should be cast to native types.
      *
@@ -36,4 +37,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function userInfo()
+    {
+        return $this->hasOne('App\Model\UserInfo');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Model\Post');
+    }
 }
