6bed9f6d32aa30049cd4b01aa4ae0da6af770a30
Guz117 <cristian.guzman117@gmail.com>
2022-03-02 17:56:01 +0100

    Correzione slug

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Model;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -39,5 +40,21 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function createSlug($title)
+    {
+        $slug = Str::slug($title, '-');
+
+        $oldPost = Post::where('slug', $slug)->first();
+
+        $counter = 0;
+        while ($oldPost) {
+            $newSlug = $slug . '-' . $counter;
+            $oldPost = Post::where('slug', $newSlug)->first();
+            $counter++;
+        }
+
+        return (empty($newSlug)) ? $slug : $newSlug;
     }
 }
