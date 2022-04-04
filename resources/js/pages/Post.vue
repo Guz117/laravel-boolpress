a7f691c7d6034a260397de922c8a53c345f5c77b
Guz117 <cristian.guzman117@gmail.com>
2022-04-04 18:56:56 +0200

    layuoy admin

--- resources/js/pages/Post.vue
+++ resources/js/pages/Post.vue
@@ -1,46 +1,59 @@
 <template>
-  <div>
-    <div class="row row-cols-1 row-cols-md-4 g-4" v-if="post">
-        <div class="card">
-         <img v-if="post.image" :src="'/storage/'+post.image" class="card-img-top" :alt="post.title">
-          <img v-else src="/storage/upload/download.png" class="card-img-top" :alt="post.title">
-          <div class="card-body">
-            <h5 class="card-title">{{ post.title }}</h5>
-            <p class="card-text">{{ post.content }}</p>
-          </div>
+    <div>
+        <div class="row row-cols-1 row-cols-md-4 g-4" v-if="post">
+            <div class="card">
+                <img
+                    v-if="post.image"
+                    :src="'/storage/' + post.image"
+                    class="card-img-top"
+                    :alt="post.title"
+                />
+                <img
+                    v-else
+                    src="/storage/upload/download.png"
+                    class="card-img-top"
+                    :alt="post.title"
+                />
+                <div class="card-body">
+                    <h5 class="card-title">{{ post.title }}</h5>
+                    <p class="card-text">{{ post.content }}</p>
+                </div>
+            </div>
         </div>
-      </div>
-      <router-link class="btn btn-info" :to="{ name: 'home' }">Home</router-link>
-      <router-link class="btn btn-info" :to="{ name: 'posts' }">Posts</router-link>
+        <router-link class="btn btn-info" :to="{ name: 'home' }"
+            >Home</router-link
+        >
+        <router-link class="btn btn-info" :to="{ name: 'posts' }"
+            >Posts</router-link
+        >
     </div>
 </template>
 
 <script>
 import Axios from "axios";
 
-  export default {
-    name: 'Post',
-    props: ['id'],
+export default {
+    name: "Post",
+    props: ["id"],
     data() {
-      return {
-        post: null
-      }
+        return {
+            post: null,
+        };
     },
     created() {
-      const url = 'http://127.0.0.1:8000/api/v1/posts/' + this.id;
-      this.getPost(url);
+        const url = "http://127.0.0.1:8000/api/v1/posts/" + this.id;
+        this.getPost(url);
     },
     methods: {
-      getPost(url){
-          Axios.get(url, {headers: {'Authorization': 'Bearer c12345g'}}).then(
-            (result) => {
-              this.post = result.data.results.data;
+        getPost(url) {
+            Axios.get(url, {
+                headers: { Authorization: "Bearer c12345g" },
+            }).then((result) => {
+                this.post = result.data.results.data;
             });
-      }
-    }
-  }
+        },
+    },
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
